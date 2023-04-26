670db6afe6edb85c4a08d17d30eec4f0b3e9fba3
smit224 <49568313+smit224@users.noreply.github.com>
2023-04-26 13:34:31 -0700

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -1,16 +1,11 @@
-import axios from "axios"; //Axios is the library which helps in Call
+import axios from "axios";
 
-
-
-
-
-
-
-export const getPlacesData = async (type, sw, ne) => {
-  try {
+const getPlacesData = async (type, sw, ne) => {
    
-    const { data: { data } } = await axios.get(`https://travel-advisor.p.rapidapi.com/${type}/list-in-boundary`, {
-
+  try {
+    const {
+      data: { data },
+    } = await axios.get(`https://travel-advisor.p.rapidapi.com/${type}/list-in-boundary/`, {
       params: {
         bl_latitude: sw.lat,
         tr_latitude: ne.lat,
@@ -18,12 +13,14 @@ export const getPlacesData = async (type, sw, ne) => {
         tr_longitude: ne.lng,
       },
       headers: {
-        'X-RapidAPI-Key': '12155806abmshb052d554e9f2c8fp1785b4jsnbd7968cba6a8',
-        'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com'
-      }
+        "X-RapidAPI-Key": "b69087ff35mshec5f892bd818426p1c5d72jsnd7603f09956a",
+        "X-RapidAPI-Host": "travel-advisor.p.rapidapi.com",
+      },
     });
     return data;
-  } catch (error) {
-    console.log("---Error---", error)
+  } catch (err) {
+    console.log("Error in PlaceData", err);
   }
-}
+};
+
+export default getPlacesData;
