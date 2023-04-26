63758f1374de6ec4362eecdc7dd0220d9be46280
smit224 <49568313+smit224@users.noreply.github.com>
2023-04-26 13:37:38 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,75 +1,74 @@
-import React, { useEffect, useState } from "react";
 import { CssBaseline, Grid } from "@material-ui/core";
-
-import Header from './components/Header/Header'
-import List from './components/List/List'
-import Map from './components/Map/Map'
-import PlaceDetails from './components/PlaceDetails/PlaceDetails'
-import { getPlacesData } from "./api";
+import Header from "./components/Header/Header";
+import Map from "./components/Map/Map";
+import List from "./components/List/List";
+import getPlacesData from "./api/index";
+import { useState, useEffect } from "react";
 
 const App = () => {
+  const [places, setPlaces] = useState([]);
+  const [coords, setCoords] = useState({});
+  const [bounds, setBounds] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [childClicked, setChildClicked] = useState(null);
+  const [type, setType] = useState("restaurants");
+  const [rating, setRating] = useState("");
+  const [filteredPlaces, setFilteredPlaces] = useState([]);
 
-    const [places, setPlaces] = useState([]);
-    const [coordinates, setCoordinates] = useState({ });
-    const [bounds, setBounds] = useState({});
-    const [childClicked, setChildClicked] = useState(null);
-    const [isLoading, setIsLoading] = useState(false);
-    const [type, setType] = useState('restaurants');
-    const [rating, setRating] = useState([]);
-    const [filteredPlaces, setFilteredPlaces] = useState([]);
-
-    useEffect(() => {
-        navigator.geolocation.getCurrentPosition(({ coords: { latitude, longitude } }) => {
-            setCoordinates({ lat: latitude, lng: longitude });
-        });
-    }, []);
-
-    useEffect(() => {
-        const filteredPlaces = places.filter((place) => place.rating > rating)
-        setFilteredPlaces(filteredPlaces)
-    }, [rating]);
-
-    useEffect(() => {
-       setIsLoading(true);
-        if (bounds){
-            
-            getPlacesData(type, bounds.sw, bounds.ne)
-            .then((data) => {
-                
-                console.log("=====data=====", data)
-                setFilteredPlaces([]);
-                setIsLoading(false);
-            })}
-    }, [type, coordinates, bounds]);
-        
-        
-    return (
-        <>
-            <CssBaseline />
-            <Header setCoordinates={setCoordinates}/>
-            <Grid container spacing={3} style={{ width: '100%' }}>
-                <Grid item xs={12} md={4}>
-                    <List places = {filteredPlaces.length ? filteredPlaces: places}
-                    childClicked={childClicked}
-                    isLoading={isLoading}
-                    type={type}
-                    setType={setType}
-                    rating={rating}
-                    setRating={setRating}
-                    />
-                </Grid>
-                <Grid item xs={12} md={8}>
-                    <Map
-                        setCoordinates={setCoordinates}
-                        setBounds={setBounds}
-                        coordinates={coordinates}
-                        places={filteredPlaces.length ? filteredPlaces: places}
-                        setChildClicked = {setChildClicked}
-                    />
-                </Grid>
-            </Grid>
-        </>
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      ({ coords: { latitude, longitude } }) => {
+        setCoords({ lat: latitude, lng: longitude });
+      }
     );
-}
+  }, []);
+
+  useEffect(() => {
+    setIsLoading(true);
+
+    if (bounds && bounds.sw && bounds.ne) {
+      getPlacesData(type, bounds.sw, bounds.ne).then((data) => {
+        setPlaces(data?.filter((place) => place.name && place.num_reviews > 0));
+        setFilteredPlaces([]);
+        setIsLoading(false);
+      });
+    }
+  }, [type, bounds]);
+
+  useEffect(() => {
+    const filtered = places.filter((place) => Number(place.rating) > rating);
+
+    setFilteredPlaces(filtered);
+  }, [rating]);
+
+  return (
+    <>
+      <CssBaseline />
+      <Header setCoords={setCoords} />
+      <Grid container spacing={3} style={{ width: "100%" }}>
+        <Grid item xs={12} md={4}>
+          <List
+            places={filteredPlaces.length ? filteredPlaces : places}
+            childClicked={childClicked}
+            isLoading={isLoading}
+            type={type}
+            setType={setType}
+            rating={rating}
+            setRating={setRating}
+          />
+        </Grid>
+        <Grid item xs={12} md={8}>
+          <Map
+            setCoords={setCoords}
+            setBounds={setBounds}
+            coords={coords}
+            places={filteredPlaces.length ? filteredPlaces : places}
+            setChildClicked={setChildClicked}
+          />
+        </Grid>
+      </Grid>
+    </>
+  );
+};
 
 export default App;
